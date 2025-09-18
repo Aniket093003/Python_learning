6bbf67c01292404a932486d6d6ca25ee99b267fa
Aniket093003 <aniketkainth98105@gmail.com>
2025-09-18 15:25:44 +0530

    loop questions

--- Practice/while_loop.py
+++ Practice/while_loop.py
@@ -14,7 +14,7 @@ while y >= 1 :
 
 # print the multiplication table of a number
 
-num = 4 
+num = int(input("Enter a number: ")) 
 i = 1
 while i <= 10 :
     print(num, "*", i, "=", num*i)
@@ -22,7 +22,26 @@ while i <= 10 :
 
     
 # print the elements using while loop 1,4,9,16,25,36,49,64,81,100   
-a = 1 
+"""a = 1 
 while a <= 10 :
     print(a * a)
     a += 1
+"""
+a = [1,4,9,16,25,36,49,64,81,100]
+inx = 0
+while inx < len(a) :
+    print(a[inx])
+    inx += 1    
+
+
+# search an element in a list using while loop
+x = int(input("Enter a number to search: "))
+found = False
+index = 0
+while index < len(a):
+    if a[index] == x:
+        found = True
+        break
+    index += 1
+if found:
+    print(f"{x} is found in the list at index {index}.")    
