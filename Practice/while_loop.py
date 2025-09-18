7012328f5fe456714463f8e9fbf77cb6c9ace847
Aniket093003 <aniketkainth98105@gmail.com>
2025-09-18 13:04:28 +0530

    while loop practice

---
+++ Practice/while_loop.py
@@ -0,0 +1,28 @@
+# print numbers from 1 to 100
+
+x = 1 
+while x <= 100 :
+    print(x)
+    x += 1
+print("x")
+# print even numbers from 100 to 1
+
+y = 100
+while y >= 1 :
+    print(y)
+    y -= 1
+
+# print the multiplication table of a number
+
+num = 4 
+i = 1
+while i <= 10 :
+    print(num, "*", i, "=", num*i)
+    i += 1   
+
+    
+# print the elements using while loop 1,4,9,16,25,36,49,64,81,100   
+a = 1 
+while a <= 10 :
+    print(a * a)
+    a += 1
