2e6c18ed5ba870448504fc29a305fc456645aba6
aniket093003 <aniketkainth98107@gmail.com>
2025-09-22 15:52:15 +0530

    functions practice in python

---
+++ Practice/functions.py
@@ -0,0 +1,53 @@
+# lst = [1,2,3,4,5,6,7]
+
+# def len_lst(lst):
+#     count = 0
+#     for i in lst:
+#         count += 1
+#     return count
+# print(len_lst(lst))
+
+
+# def lst_line(lst):
+#     for i in lst:
+#         print(i, end=" ")
+        
+
+# lst_line(lst)
+
+
+# def fact(n):
+#     factorial = 1
+#     for i in range(1, n+1):
+#         factorial *= i
+#     print(factorial)
+#     fact(n-1)
+# fact(5)
+
+
+
+# def usd_to_inr(usd):
+#     inr = usd * 82.74
+#     return inr
+
+# print(usd_to_inr(100))
+
+
+# sum for first n natural numbers using recursion
+# def fact(n):
+#     if (n == 1):
+#         return 1
+#     else:
+#         return n+fact(n-1)
+# print(fact(20))
+
+
+lst = [1,2,3,4,5,6,7]
+
+def rec_fn(lst, idx=0):
+
+    if (idx == len(lst)):
+        return
+    print(lst[idx])
+    rec_fn(lst, idx+1)
+rec_fn(lst)
