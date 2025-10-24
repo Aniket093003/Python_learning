b70ce520e93e67e3379db81c6949251af96b800d
aniket093003 <aniketkainth98105@gmail.com>
2025-10-24 12:56:53 +0530

    file handeling learning

--- File_handeling/ques2/main.py
+++ File_handeling/ques2/main.py
@@ -3,23 +3,23 @@ f2 = "b.txt"
 f3 = "c.txt"
 
 with open(f1, "w") as f:
-    f.write("10")
+    f.write(input("enter value of a: "))
 with open(f1 , 'r') as f:
     f1_data = int(f.read())
 print("file 1 created")
 
 with open(f2, "w") as f:
-    f.write("5")
+    f.write(input("enter value of b: "))
 with open(f2 , 'r') as f:
     f2_data = int(f.read())
 
 print("file 2 created")
 
-
+f3_data = f1_data + f2_data
 
 with open(f3, "w") as f:
-    f.write(str(f1_data + f2_data))
+    f.write(str(f3_data))
 
-print("file 3 created")
+print("file 3 created and result is :", f3_data )
 
 
---
+++ Libary_managemant_system/main.py
@@ -0,0 +1,9 @@
+# Library Management System
+
+# Concepts used: Classes, lists of objects, file handling (optional)
+# Description:
+# Create classes Book and Library.
+
+# Add books, remove books, search by title or author.
+
+# Keep track of issued and returned books.
