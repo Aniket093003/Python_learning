e5132b82e62f3e902b9af240ddcc20d883e46a9a
aniket093003 <aniketkainth98105@gmail.com>
2025-11-04 12:53:03 +0530

    regular expressions

---
+++ Practice/regex.py
@@ -0,0 +1,24 @@
+import re 
+txt = "afkjhaeiu faejfn kwrq4 fajfb"
+
+# result = re.findall("\d" , txt)
+# print(result)
+## 2
+# txt = "9317093166"
+# if re.fullmatch(r"^\d{10}",txt):
+#     print("true")
+# else:
+#     print("false")
+### 3
+# words = re.findall(r"\D", txt)
+
+# print(words)
+###4
+# cap_words = re.findall(r"[A-Z]", txt)
+# print(cap_words)
+# pattern = r"\w+@+\w+.\w+"
+# res = re.findall(pattern , txt)
+# print(res)
+###5
+res = re.sub("\w", "#", txt)
+print(res)
